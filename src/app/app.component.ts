4e6f19b2702e39c78e536f0528abc4916e58dc0d
MRVirus007 <hackerpc77@gmail.com>
2022-06-19 11:41:59 +0530

    icons and splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { App } from '@capacitor/app';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform,
+    private router: Router) {
+      this.platform.backButton.subscribeWithPriority(-1, () => {
+        if (this.router.url === '/notes') {
+            App.exitApp();
+        }
+    });
+    }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { NotesService } from '../services/notes.service';
 @Component({
   selector: 'app-home',
@@ -10,7 +11,9 @@ export class HomePage {
   public isSearchbarOpened  = false;
   searchText: string;
   colors = ['#3880ff', '#3dc2ff', '#5260ff', '#2dd36f', '#ffc409', '#eb445a', '#92949c'];
-  constructor(public notesService: NotesService, private router: Router){}
+  constructor(public notesService: NotesService,
+     private router: Router,
+     private alertController: AlertController){}
   ionViewDidEnter() {
     this.notesService.getAllNotes();
   }
@@ -29,5 +32,32 @@ export class HomePage {
   }
   goToEdit(id) {
     this.router.navigate(['/edit', id]);
+  }
+  async showAppInfo() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-alert-about',
+      header: 'App Info',
+      subHeader: 'Design Inspired By - Divya Kelaskar',
+      message: 'Developed By - Mohammed Rokerya, This app is developed in Ionic Framework with Angular.',
+      buttons: [{
+        text:'About Developer',
+        role: 'discard',
+        cssClass: 'danger-class',
+        handler: () => {
+          window.open('https://in.linkedin.com/in/mohammed-rokerya-3346a3144','_system','location=yes');
+        }
+      },
+      {
+        text:'Design Info',
+        role: 'save',
+        cssClass: 'success-class',
+        handler: () => {
+          window.open('https://www.figma.com/community/file/1014161465589596715','_system','location=yes');
+        }
+      }]
+    });
+
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
   }
 }
